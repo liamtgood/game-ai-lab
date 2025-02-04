5118d0b4c1fa759abd6e97656c589b32a4973b78
liamtgood <liamgood83@gmail.com>
2025-02-04 12:05:30 -0500

    lab 04 commmit

--- lab04/lab04.py
+++ lab04/lab04.py
@@ -20,8 +20,17 @@ def run_console_chat(sign, **kwargs):
                 print('Ending match:', ending_match)
             break
 
-lab04_params = {}
+lab04_params = { 
+                "template_file":"lab04/lab04_trader_chat.json",
+                "sign" : 'Liam',
+                "end_regex": 'Trader(.)DONE'
+                }
 
 if __name__ ==  '__main__':
     # run lab04.py to test your template interactively
-    pass
+    trader_template_file = 'lab04/lab04_trader_chat.json'
+    run_console_chat(template_file=trader_template_file,
+                     inventory = '[Mana Potion, Health Potion, Iron Sword]',
+                     sign = 'Liam',
+                     end_regex = r'ORDER(.*)DONE')
+    
