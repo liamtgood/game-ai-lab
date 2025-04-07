268155075c4060c7da9f26bee4051717ca58f2d3
liamtgood <liamgood83@gmail.com>
2025-04-06 20:05:14 -0400

    more tools

--- Project Stuff/Project01.py
+++ Project Stuff/Project01.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 import sys
 sys.path.append(str(Path(__file__).parents[1]))
-
+import random
 from ollama import chat
 from util.llm_utils import run_console_chat, tool_tracker, TemplateChat, pretty_stringify_chat, ollama_seed as seed
 
-
+'''
 #model 
 sign_your_name = 'Liam Good'
 model = 'llama3.2'
@@ -19,20 +19,57 @@ messages = [{'role': 'system', 'content': 'You should have emotions like a human
 messages.append({'role': 'assistant', 'content': 'Are you ready to begin your adventure? '})
 options = {'temperature': 0.5, 'max_tokens': 10}
 response = chat(model = model, messages = messages, stream = False, options = options)
-
+'''
 # create the player character - name, class, stats, starting items
 #messages.append({'role':'assistant','content':'Let\'s begin Character Creation! Chose A class: \n -Fighter \n -Wizard \n -Rogue \n -Cleric \n -Bard\n'})
 #options = {'temperature': 0.5, 'max_tokens': 10}
 #response = chat(model = model, messages = messages, stream = False, options = options)
 
+@tool_tracker
+def process_function_call(function_call):
+    name = function_call.name
+    args = function_call.arguments
+
+    return globals()[name](**args)
+
+def roll_for(skill, dc, player):
+    n_dice = 1
+    sides = 20
+    roll = sum([random.randint(1, sides) for _ in range(n_dice)])
+    if roll >= int(dc):
+        return f'{player} rolled {roll} for {skill} and succeeded!'
+    else:
+        return f'{player} rolled {roll} for {skill} and failed!'
+
+def process_response(self, response):
+    # Fill out this function to process the response from the LLM
+    # and make the function call 
+    #chat = TemplateChat.from_file(self)
+    #response = chat.completion(**{'ask': 'Roll for athletic skill to see if the player can pass over the ravine'})
+
+    #defaultdict(<class 'list'>, {'process_function_call_calls': [{'name': 'process_function_call', 'args': (Function(name='roll_for', arguments={'dc': 5, 'player': 'adventurer', 'skill': 'Athletics'}),), 'kwargs': {}, 'result': 'adventurer rolled 7 for Athletics and succeeded!'}]}) 
+
+    if response.message.tool_calls:
+        self.messages.append({'role': 'tool',
+                          'name': response.message.tool_calls[0].function.name, 
+                          'arguments': response.message.tool_calls[0].function.arguments,
+                          'content': process_function_call(response.message.tool_calls[0].function)
+                         })
+        response = self.completion()
+    return response
+
+
+run_console_chat(template_file='Project Stuff\dice.json',
+                 process_response=process_response)
 
 
 # once player selects a class use rag to describe that classes abilities
 
 
 # starts adventure
-options |= {'seed': seed(sign_your_name)}
+#options |= {'seed': seed(sign_your_name)}
 # Chat loop
+'''
 while True:
   response = chat(model=model, messages=messages, stream=False, options=options)
   # chat system
@@ -43,6 +80,6 @@ while True:
   # 
   if messages[-1]['content'] == '/exit':
     break
-  
+'''
 
 
